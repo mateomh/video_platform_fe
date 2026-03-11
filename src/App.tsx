3a470b149f2d05c8f5e77ce08d14091ef70a2791
Mateo mojica <mmojicah.84@gmail.com>
2026-03-10 20:48:41 -0500

    create main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./assets/stylesheets/variables.css";
+import "./assets/stylesheets/base.css";
+import "./assets/stylesheets/navbar.css";
+import "./assets/stylesheets/messages.css";
+import "./assets/stylesheets/buttons.css";
+import "./assets/stylesheets/forms.css";
 
-function App() {
+
+export default function App() {
   return (
     <>
-      
+      <Navbar />
+      <main className="main-content">
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./components/context/AuthContext";
+import { MessagesProvider } from "./components/context/MessagesContext";
+import AppRoutes from './routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <AuthProvider>
+        <MessagesProvider>
+          <AppRoutes />
+        </MessagesProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
